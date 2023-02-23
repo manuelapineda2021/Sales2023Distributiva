8a6cacb0d5c57ed664fb66cf8a9f69305543458c
Manuela Pineda <manup102010@hotmail.com>
2023-02-23 00:52:31 -0500

    Add data context

---
+++ Sales.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions <DataContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        //Validate duplicates to Country table
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique(); // create index and unique for Country table
+        }
+    }
+}
