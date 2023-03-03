fd253dca39a5f54fbc2c46b585f727c8ec0385cd
Manuela Pineda <manup102010@hotmail.com>
2023-03-02 21:06:48 -0500

    complete country and category API controller

--- Sales.API/Controllers/CategoriesController.cs
+++ Sales.API/Controllers/CategoriesController.cs
@@ -23,12 +23,46 @@ namespace Sales.API.Controllers
             return Ok(await _context.Categories.ToListAsync());
         }
 
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
         [HttpPost]
         public async Task<ActionResult> PostAsync(Category category)
         {
             _context.Add(category);
             await _context.SaveChangesAsync();
             return Ok(category);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+            return Ok(category);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -17,11 +17,24 @@ namespace Sales.API.Controllers
             _context = context;
         }
 
-        //Add records
+        //Consult records
         [HttpGet]
         public async Task<IActionResult> GetAsync()
         {
             return Ok(await _context.Countries.ToListAsync());
+        }
+
+        //Query a record with Id
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
         }
 
         //Add new record
@@ -31,6 +44,29 @@ namespace Sales.API.Controllers
             _context.Add(country); //sends records to the database
             await _context.SaveChangesAsync(); //saves records in the database
             return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);//update country
+            await _context.SaveChangesAsync(); //save country update
+            return Ok(country);
+        }
+
+        //Delete country
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
